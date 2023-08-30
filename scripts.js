528a2623cad7833cffc650ac3dca908b34275cde
Gintautas Poderys <gintautasp25@gmail.com>
2023-08-30 17:57:42 +0300

    made basic 50x50 window sketch

--- scripts.js
+++ scripts.js
@@ -1,14 +1,27 @@
-const body = document.querySelector('body')
-for(let i= 0; i<4; i++) {
+let squareLength = 50
+
+const body = document.querySelector('.sketch-window')
+for(let i= 0; i<squareLength; i++) {
     const makeDiv = document.createElement('div')
     makeDiv.classList.add('container')
     body.appendChild(makeDiv)
 }
+
 const container = document.querySelectorAll('.container')
 container.forEach(container => {
-    for(let i= 0; i<4; i++) {
+    for(let i= 0; i<squareLength; i++) {
     const makeDiv = document.createElement('div')
     makeDiv.classList.add('container-item')
     container.appendChild(makeDiv)
 }
 }) 
+
+const containerItem = document.querySelectorAll('.container-item')
+for (let i = 0; i < containerItem.length; i++){
+    // containerItem[i].style.maxHeight = 
+}
+
+addEventListener("mouseover", (event) => {
+    const text = console.log(event.target)
+    event.target.classList.add('colored')
+});
